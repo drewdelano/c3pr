dc8b26871f2f571a053caa5c92705f647c5f597d
Drew <ddelanomailbox-spare@yahoo.com>
2021-12-05 15:20:40 -0800

    Example for github actions with placeholders and docs

--- src/C3PR.Core/Services/ExternalBuildTrigger.cs
+++ src/C3PR.Core/Services/ExternalBuildTrigger.cs
@@ -14,7 +14,22 @@ namespace C3PR.Core.Services
     {
         public async Task TriggerBuild(SlackMessageStorage storage)
         {
-            throw new NotImplementedException();
+            // Example of how to configure this for Github Actions repository_dispatch
+            // https://pakstech.com/blog/github-actions-repository-dispatch/
+            // Replace all PLACEHOLDER_ s in this file with the appropriate data
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "PLACE_HOLDER_FOR_GITHUB_TOKEN");
+            client.DefaultRequestHeaders.Host = "api.github.com";
+            client.DefaultRequestHeaders.UserAgent.Add(ProductInfoHeaderValue.Parse("C3PR"));
+
+            var postBody = "{\"event_type\": \"c3pr_build\", \"client_payload\": { \"title\": \"title\" }}";
+            var result = await client.PostAsync("https://api.github.com/repos/PLACE_HOLDER_FOR_ORG/PLACEHOLDER_FOR_PROJECT/dispatches", new StringContent(postBody));
+
+            if (!result.IsSuccessStatusCode)
+            {
+                throw new Exception("Report that things didn't work as planned");
+            }
         }
     }
 }
